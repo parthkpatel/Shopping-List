31acdc042be94f0988edfc6c3de98e073522f615
Parth Patel <parth.patel@sageworks.com>
2020-02-12 18:43:39 -0500

    Committing Backend code for the Shopping List

---
+++ routes/api/items.js
@@ -0,0 +1,37 @@
+const router = require('express').Router();
+
+ // Item Model
+ const Item = require('../../models/item.model');
+
+// @route   GET api/items
+// @desc    Get All Items
+// @access  Public
+router.get('/', (req, res) => {
+    Item.find()
+        .sort({ date: -1 })
+        .then(items => res.json(items))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// @route   POST api/items
+// @desc    Create An Item
+// @access  Public
+router.post('/', (req, res) => {
+    const name = req.body.name;
+    const newItem = new Item({name})
+
+    newItem.save()
+        .then(item => res.json(item))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// @route   DELETE api/items/:id
+// @desc    Delete An Item
+// @access  Public
+router.delete('/:id', (req, res) => {
+    Item.findByIdAndDelete(req.params.id)
+        .then(() => res.json({ success: true}))
+        .catch(err => res.status(404).json({ success: false }));
+});
+
+ module.exports = router;
