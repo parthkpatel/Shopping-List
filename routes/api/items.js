5547051b2fbaba67f5ca8b52362eeeead2109769
Parth Patel <parth.patel@sageworks.com>
2020-02-15 12:07:39 -0500

    Implementing JWT authentication, adding authentication to items routes

--- routes/api/items.js
+++ routes/api/items.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const auth = require('../../middleware/auth');
 
  // Item Model
  const Item = require('../../models/item.model');
@@ -15,8 +16,8 @@ router.get('/', (req, res) => {
 
 // @route   POST api/items
 // @desc    Create An Item
-// @access  Public
-router.post('/', (req, res) => {
+// @access  Private
+router.post('/', auth, (req, res) => {
     const name = req.body.name;
     const newItem = new Item({name})
 
@@ -27,8 +28,8 @@ router.post('/', (req, res) => {
 
 // @route   DELETE api/items/:id
 // @desc    Delete An Item
-// @access  Public
-router.delete('/:id', (req, res) => {
+// @access  Private
+router.delete('/:id', auth, (req, res) => {
     Item.findByIdAndDelete(req.params.id)
         .then(() => res.json({ success: true}))
         .catch(err => res.status(404).json({ success: false }));
--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const path = require('path');
 
 const items = require('./routes/api/items');
+const users = require('./routes/api/users');
+const auth = require('./routes/api/auth');
+const config = require('config');
 
 const app = express();
 
 // Bodyparser Middleware
-app.use(bodyParser.json());
+app.use(express.json());
 
 // DB Config
-const db = require('./config/keys').mongoURI;
+const db = config.get('mongoURI');
 
 // Connect to MongoDB
 mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
@@ -20,6 +22,8 @@ mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTo
 
 // Use Routes
 app.use('/api/items', items);
+app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
