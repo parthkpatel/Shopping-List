b0cb11973ae8b0be155bd2742a92aa1f9d46db16
Parth Patel <parth.patel@sageworks.com>
2020-02-18 18:42:01 -0500

    Implementing Authentication and Error Actions and Reducers for the Frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
-import React, { Component }  from 'react';
-import AppNavbar from './components/AppNavbar';
-import ShoppingList from './components/ShoppingList';
-import ItemModal from './components/ItemModal'
-import { Container } from 'reactstrap';
+import React, { Component } from "react";
+import AppNavbar from "./components/AppNavbar";
+import ShoppingList from "./components/ShoppingList";
+import ItemModal from "./components/ItemModal";
+import { Container } from "reactstrap";
 
-import { Provider } from 'react-redux';
-import store from './store';
+import { Provider } from "react-redux";
+import store from "./store";
+import { loadUser } from "./actions/authActions";
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 class App extends Component {
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
   render() {
     return (
       <Provider store={store}>
@@ -18,11 +22,11 @@ class App extends Component {
           <AppNavbar />
           <Container>
             <ItemModal />
-            <ShoppingList/>
+            <ShoppingList />
           </Container>
-        </div>        
+        </div>
       </Provider>
-    )
+    );
   }
 }
 
