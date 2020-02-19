d91eac59aecfddc1f1c96070993ffc9956eddf2f
Parth Patel <parth.patel@sageworks.com>
2020-02-19 15:51:19 -0500

    Implementing Registration and Logout on Frontend, adjusting Actions/Reducers to account for these implementations

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -20,7 +20,7 @@ export const loadUser = () => (dispatch, getState) => {
   axios
     .get("/api/auth/user", tokenConfig(getState))
     .then(res =>
-      dispatchEvent({
+      dispatch({
         type: USER_LOADED,
         payload: res.data
       })
@@ -33,7 +33,44 @@ export const loadUser = () => (dispatch, getState) => {
     });
 };
 
-// Setup config/headers and taken
+// Register User
+export const register = ({ name, email, password }) => dispatch => {
+  // Headers
+  const config = {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+
+  // Request body
+  const body = JSON.stringify({ name, email, password });
+
+  axios
+    .post("/api/users", body, config)
+    .then(res =>
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: res.data
+      })
+    )
+    .catch(err => {
+      dispatch(
+        returnErrors(err.response.data, err.response.status, "REGISTER_FAIL")
+      );
+      dispatch({
+        type: REGISTER_FAIL
+      });
+    });
+};
+
+// Logout User
+export const logout = () => {
+  return {
+    type: LOGOUT_SUCCESS
+  };
+};
+
+// Setup config/headers and token
 export const tokenConfig = getState => {
   // Get token from localstorage
   const token = getState().auth.token;
@@ -45,7 +82,7 @@ export const tokenConfig = getState => {
     }
   };
 
-  // If token, add to headers;
+  // If token, add to headers
   if (token) {
     config.headers["x-auth-token"] = token;
   }
--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -1,47 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink,
-    Container
-} from 'reactstrap';
-
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container
+} from "reactstrap";
+import RegisterModal from "./auth/RegisterModal";
+import Logout from "./auth/Logout";
 class AppNavbar extends Component {
-    state = {
-        isOpen: false
-    }
-
-    toggle = () => {
-        this.setState({
-            isOpen: !this.state.isOpen
-        })
-    }
-
-    render() {
-        return (
-            <div>
-            <Navbar color="dark" dark expand="sm" className="mb-5">
-                <Container>
-                    <NavbarBrand href="/">ShoppingList</NavbarBrand>
-                    <NavbarToggler onClick={this.toggle} />
-                    <Collapse isOpen={this.state.isOpen} navbar>
-                        <Nav className="ml-auto" navbar>
-                            <NavItem>
-                                <NavLink href="https://github.com/parthkpatel">
-                                    Github
-                                </NavLink>
-                            </NavItem>
-                        </Nav>
-                    </Collapse>
-                </Container>
-            </Navbar>
-            </div>
-        );
-    }
+  state = {
+    isOpen: false
+  };
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  };
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" dark expand="sm" className="mb-5">
+          <Container>
+            <NavbarBrand href="/">ShoppingList</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <RegisterModal />
+                </NavItem>
+                <NavItem>
+                  <Logout />
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );
+  }
 }
-
 export default AppNavbar;
